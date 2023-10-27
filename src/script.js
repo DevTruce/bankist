214a57eb7322ae323a6a5413fb7a743b2e0759b0
DevTruce <DevTruce@Gmail.com>
2023-10-27 19:35:50 -0400

    updated all number to round to 2 nearest decimal points

--- src/script.js
+++ src/script.js
@@ -7,34 +7,46 @@
 /////////////////////////////////////////////////
 //// DATA
 const account1 = {
-  owner: 'Truce Ramcharitar',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300], // transactions
+  owner: 'Truce R',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
-  owner: 'Meagan M',
+  owner: 'Meagan R',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Johnny Test',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Timmy Turner',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 /////////////////////////////////////////////////
 //// ELEMENTS
@@ -70,7 +82,7 @@ const createUsernames = function (accs) {
     acc.username = acc.owner
       .toLowerCase()
       .split(' ')
-      .map(name => name[0].toUpperCase())
+      .map(name => name[0].toLowerCase())
       .join('');
   });
 };
@@ -92,7 +104,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">$${Math.abs(mov)}</div>
+        <div class="movements__value">$${mov.toFixed(2)}</div>
       </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html); // fill container
@@ -103,7 +115,7 @@ const displayMovements = function (movements, sort = false) {
 //// CALCULATE AND DISPLAY TOTAL BALANCE
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((accu, cur) => accu + cur, 0);
-  labelBalance.textContent = `$${Number(acc.balance)}`;
+  labelBalance.textContent = `$${Number(acc.balance.toFixed(2))}`;
 };
 
 /////////////////////////////////////////////////
@@ -114,13 +126,13 @@ const calcDisplaySummary = function (acc) {
   const income = acc.movements
     .filter(mov => mov > 0)
     .reduce((accu, cur) => accu + cur, 0);
-  labelSumIn.textContent = `$${income}`;
+  labelSumIn.textContent = `$${income.toFixed(2)}`;
 
   // outgoing
   const outgoing = acc.movements
     .filter(mov => mov < 0)
     .reduce((accu, cur) => accu + cur, 0);
-  labelSumOut.textContent = `$${Math.abs(outgoing)}`;
+  labelSumOut.textContent = `$${outgoing.toFixed(2)}`;
 
   // interest
   let interest = acc.movements
@@ -129,8 +141,7 @@ const calcDisplaySummary = function (acc) {
     .filter(int => int >= 1)
     .reduce((accu, int) => accu + int, 0);
 
-  interest = Math.abs(interest);
-  labelSumInterest.textContent = `$${Math.round(interest)}`;
+  labelSumInterest.textContent = `$${interest.toFixed(2)}`;
 };
 
 // calcDisplaySummary(account1.movements);
@@ -225,7 +236,7 @@ btnTransfer.addEventListener('click', function (event) {
 btnLoan.addEventListener('click', function (event) {
   event.preventDefault(); // diable form submit
 
-  const amount = inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
     currentAccount.movements.push(Number(amount)); // add loan to user
